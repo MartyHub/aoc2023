7eb618200d26d6c93a6296ee3ad5b7ffe0b6991b
Julien GOTTELAND <julien.gotteland@gmail.com>
2023-12-03 12:42:15 +0100

    Add daily template

---
+++ template/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	// aoc2023.Expect("Sample 1", do("dayXX/data/sample.txt"), 0)
+	// aoc2023.Expect("Part 1", do("dayXX/data/input.txt"), 0)
+}
+
+func part2() {
+	// aoc2023.Expect("Sample 2", do("dayXX/data/sample.txt"), 0)
+	// aoc2023.Expect("Part 2", do("dayXX/data/input.txt"), 0)
+}
