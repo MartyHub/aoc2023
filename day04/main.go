d43e075dd7a8ef5a4f7ca3ce97c5b15613332a9a
Julien GOTTELAND <julien.gotteland@gmail.com>
2023-12-04 08:26:35 +0100

    Day 4

---
+++ day04/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/MartyHub/aoc2023"
+)
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	aoc2023.Expect("Sample 1", parse("day04/data/sample.txt").Score(), 13)
+	aoc2023.Expect("Part 1", parse("day04/data/input.txt").Score(), 20667)
+}
+
+func part2() {
+	aoc2023.Expect("Sample 2", parse("day04/data/sample.txt").Scratchcards(), 30)
+	aoc2023.Expect("Part 2", parse("day04/data/input.txt").Scratchcards(), 5833065)
+}
+
+func parse(filePath string) Cards {
+	var res Cards
+
+	for i, line := range aoc2023.MustReadLines(filePath) {
+		line = strings.SplitN(line, ":", 2)[1]
+		nums := strings.SplitN(line, "|", 2)
+
+		res = append(res, Card{
+			id:      i + 1,
+			gotNums: parseNums(nums[1]),
+			winNums: parseNums(nums[0]),
+		})
+	}
+
+	return res
+}
+
+func parseNums(s string) map[int]struct{} {
+	re := regexp.MustCompile(`(\d+)`)
+	res := make(map[int]struct{})
+
+	for _, match := range re.FindAllString(s, -1) {
+		res[aoc2023.Must(strconv.Atoi(match))] = struct{}{}
+	}
+
+	return res
+}
+
+type (
+	Card struct {
+		id      int
+		gotNums map[int]struct{}
+		winNums map[int]struct{}
+	}
+
+	Cards []Card
+)
+
+func (card Card) WinCount() int {
+	res := 0
+
+	for num := range card.gotNums {
+		if _, found := card.winNums[num]; found {
+			res++
+		}
+	}
+
+	return res
+}
+
+func (card Card) Score() int {
+	res := 0
+
+	for num := range card.gotNums {
+		if _, found := card.winNums[num]; found {
+			if res == 0 {
+				res = 1
+			} else {
+				res *= 2
+			}
+		}
+	}
+
+	return res
+}
+
+func (cards Cards) Score() int {
+	res := 0
+
+	for _, card := range cards {
+		res += card.Score()
+	}
+
+	return res
+}
+
+func (cards Cards) Scratchcards() int {
+	scratchcards := make(map[int]int)
+
+	for _, card := range cards {
+		scratchcards[card.id]++
+
+		for i := 1; i <= card.WinCount(); i++ {
+			scratchcards[card.id+i] += scratchcards[card.id]
+		}
+	}
+
+	res := 0
+
+	for _, nb := range scratchcards {
+		res += nb
+	}
+
+	return res
+}
